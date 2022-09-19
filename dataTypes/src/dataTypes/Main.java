aa32cb0647343c3e60d1d9b012ae54c28a5c06de
AHMET TARIK SERİN <ahmettarikserin@gmail.com>
2022-09-19 17:07:44 +0300

    firstweek

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,23 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		byte sayi = 12;
+	sayi = 127;   //byte 127'den büyük -128'den küçük olamaz 
+	     
+	int sayi2 = 15;
+       sayi2 = 155;
+       
+    double sayi3 = 17.77;
+     sayi3 = -129;
+     
+     String sehir = "Ankara";
+     char karakter = 'A'; //tek tırnak ve tek karakter 
+     
+     boolean dogruMu = false;
+     
+     
+	}
+
+}
